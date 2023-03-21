1d53a7e827b7fde347ede9d6ede72dfb52fe8f3c
Hunter Evangelista <hunterjevangelista@gmail.com>
2023-03-21 13:59:52 -0700

    added celsius and fahrenheit conversion functions

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+// (x - 32) * 5/9
+  const celsius = ((fahrenheit - 32) * (5/9));
+  return Math.round(celsius * 10)/10;
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  // x * (9/5) + 32
+  const fahrenheit = (celsius * (9/5) + 32);
+  return Math.round(fahrenheit * 10)/10;
 };
 
 // Do not edit below this line
