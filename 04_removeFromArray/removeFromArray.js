f24d588464a1b92af5d01ed72bba807647027e50
Hunter Evangelista <hunterjevangelista@gmail.com>
2023-03-21 13:12:04 -0700

    implemetned removeFromArray function

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,12 @@
-const removeFromArray = function() {
-
-};
+const removeFromArray = function(array, ...removeArgs) {
+    
+    if(Array.isArray(array)){
+        // you can use includes inside the method argument
+        // use ! to specify not
+        const output = array.filter(array => !removeArgs.includes(array));
+        return output;
+    }
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
