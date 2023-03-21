cadafa6fe02631549e7237627b5c007868226bea
Hunter Evangelista <hunterjevangelista@gmail.com>
2023-03-20 18:51:40 -0700

    added repeatString function

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if(num === 0) {
+        return "";
+    } else if(num < 0) {
+        return "ERROR";
+    } else {
+        let output = "";
+        
+        for(num; num > 0; num--) {
+            output = output + string;
+        }
+        return output;
+        
+    }
 };
 
 // Do not edit below this line
