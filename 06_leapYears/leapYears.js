f9a62e6ba557fab6a90f6ef24552c1c05f6ef803
Hunter Evangelista <hunterjevangelista@gmail.com>
2023-03-21 13:47:47 -0700

    added leapYears function

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,15 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    try {
+        if(year % 100 === 0 && year % 400 != 0) {
+            return false;
+        } else if(year % 4 === 0 || year % 400 === 0) {
+            return true;
+        } else {
+            return false;
+        }
+    } catch(err) {
+        return "ERROR";
+    }
 
 };
 
